73abd590229ad63fd6b4c4d6b189dfae0b7091b8
Simone Dalmasso <simone.dalmasso@gmail.com>
2013-10-09 13:49:07 +0200

    use full facets

---
+++ sy_template/views.py
@@ -0,0 +1,28 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+from geonode.layers.models import Layer
+from geonode.maps.models import Map
+from geonode.documents.models import Document
+from geonode.people.models import Profile
+from geonode.search.views import search_api
+
+def index(request, template='index.html'):
+    post = request.POST.copy()
+    post.update({'type': 'layer'})
+    request.POST = post
+    return search_page(request, template=template)
+
+
+def search_page(request, template='search/search.html', **kw): 
+    results, facets, query = search_api(request, format='html', **kw)
+
+    facets = {      
+        'maps' : Map.objects.count(),
+        'layers' : Layer.objects.count(),
+        'documents': Document.objects.count(),
+        'users' : Profile.objects.count()
+    }
+    
+    return render_to_response(template, RequestContext(request, {'object_list': results, 
+        'facets': facets}))
